330be69ae0b31c34871ff6441417e4cb8cc073fd
Talha <talhaarain666@gmail.com>
2024-04-09 22:50:46 +0500

    added multiple todo functionality

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todo = [];
+let condition = true;
+
+
+while(condition){
+
+    let todoQuestions = await inquirer.prompt(
+        [
+            {
+                name: "firstQuestion",
+                type: "input",
+                message: "What would you like to add in your todo?"
+            },
+            {
+                name: "secondQuestion",
+                type: "confirm",
+                message: "Would you like to add more in your todo?",
+                default: "true"
+            }
+        ]
+    )
+    
+    todo.push(todoQuestions.firstQuestion);
+    console.log(todo)
+    condition = todoQuestions.secondQuestion;
+}
